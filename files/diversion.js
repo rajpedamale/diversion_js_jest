fe6fccad8bc86a018271c9a8b7aa2a7d11b755c2
x1sb1W <x1sb1W@cyber-dojo.org>
2023-02-18 22:02:26 +0000

    7 ran tests, predicted green, got red

--- files/diversion.js
+++ files/diversion.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function diversion(binaryNumberLength) {
-  return 1;
+    if (binLength < 2) return binLength;
+    const numbersEndingWith0 = new Array(binLength);
+    const numbersEndingWith1 = new Array(binLength);
+    numbersEndingWith0[0] = 1;
+    numbersEndingWith1[0] = 1;
+
+    for (let i = 1; i<binLength; i++) {
+        numbersEndingWith0[i] = numbersEndingWith0[i-1] + numbersEndingWith1[i-1];
+        numbersEndingWith1[i] = numbersEndingWith0[i-1];
+    }
+    return numbersEndingWith0[binLength -1] + numbersEndingWith1[binLength - 1];
 }
 
 module.exports = diversion;
